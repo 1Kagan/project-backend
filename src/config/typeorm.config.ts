888ebdf1c27c3de61300580d1561be2ac0996095
1Kagan <kaganklc44@gmail.com>
2023-01-01 23:07:17 +0300

    .env installed. Sensitive files hidden

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,13 +11,12 @@ static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
    
    return {
 
-        host:'localhost',
-        port: 3306,
-        username: 'root',
-        password: ''
-
-
-
+        type:'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
    };
 
  }
