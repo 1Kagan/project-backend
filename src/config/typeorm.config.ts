0cd10397459bd3d7f85b97ccc90ce8a12ee49042
1Kagan <kaganklc44@gmail.com>
2023-01-01 22:45:39 +0300

    Typeorm Async was integrated into the structure. -- THE OPERATION IS COMPLETED

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,10 +1,38 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
+import { TypeOrmModuleAsyncOptions,TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
 
-    host:'localhost',
-    port: 3306,
-    username: 'root',
-    password: ''
+
+
+export default class TypeOrmConfig {
+
+static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+   
+   return {
+
+        host:'localhost',
+        port: 3306,
+        username: 'root',
+        password: ''
+
+
+
+   };
+
+ }
 
 }
+
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+
+    imports:[ConfigModule],
+
+    useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => TypeOrmConfig.
+
+    getOrmConfig(configService),
+
+    inject:[ConfigService],
+
+};
